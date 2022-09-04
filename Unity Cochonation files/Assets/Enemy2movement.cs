2113d73df25b694d1e0dd216e5121fd9984cdf43
Imane BOUROUYNE (MA/FR) <110111059+Curiosity10001@users.noreply.github.com>
2022-09-04 23:10:00 +0200

    random rotation: multiple method test

--- Unity Cochonation files/Assets/Enemy2movement.cs
+++ Unity Cochonation files/Assets/Enemy2movement.cs
@@ -6,9 +6,14 @@ public class Enemy2movement : MonoBehaviour
 {
     public Transform _parent;
     public GameObject _bombes;
-    public float _speed = 50;
+    public float _speed = 100f;
     public Rigidbody _rigidbodyE2;
-    
+    public float _rotationSpeed = 50.0f;
+
+
+    private Quaternion _Rotationdirection;
+    private float _rotationTimer = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +23,18 @@ public class Enemy2movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        _rotationTimer += Time.deltaTime;
         Object.Instantiate(_bombes, _parent);
+
+        if (_rotationTimer > 1)
+        {
+            transform.rotation = Quaternion.Slerp(Random.rotation, Random.rotation, _rotationTimer);
+            _rotationTimer = 0.0f;
+        }
     }
     private void FixedUpdate()
     {
-        this.transform.rotation = Random.rotation;
-        _rigidbodyE2.velocity = transform.forward*_speed;
+      
+        _rigidbodyE2.velocity = transform.forward * _speed;
     }
 }
