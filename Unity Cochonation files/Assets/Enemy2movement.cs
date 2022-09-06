f24a673cc0fd23de8abd90b0947b85bb5a37b2c6
Imane BOUROUYNE (MA/FR) <110111059+Curiosity10001@users.noreply.github.com>
2022-09-06 12:45:28 +0200

    Enemy movoments +spawning script redone
    bug corrections

--- Unity Cochonation files/Assets/Enemy2movement.cs
+++ Unity Cochonation files/Assets/Enemy2movement.cs
@@ -8,33 +8,54 @@ public class Enemy2movement : MonoBehaviour
     public GameObject _bombes;
     public float _speed = 100f;
     public float _Rotationfracture = 0.27f;
-   
-    
-    
+    public Rigidbody _rgdbdEnnemy2;
+
+    private Transform _ArenaSide1X;
+    private Transform _ArenaSide2X;
+    private Transform _ArenaSide3Z;
+    private Transform _ArenaSide4Z;
+    private float _X;
+    private float _Z;
+
 
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        _ArenaSide1X = GameObject.FindGameObjectWithTag("WALL1").transform;
+        _ArenaSide2X = GameObject.FindGameObjectWithTag("WALL2").transform;
+        _ArenaSide3Z = GameObject.FindGameObjectWithTag("WALL3").transform;
+        _ArenaSide4Z = GameObject.FindGameObjectWithTag("WALL4").transform;
     }
 
     // Update is called once per frame
 
-    
+    private void Awake()
+    {
+        _X = Random.Range(_ArenaSide1X.position.x + 5, _ArenaSide2X.position.x - 5);
+        _Z = Random.Range(_ArenaSide3Z.position.z + 5, _ArenaSide4Z.position.z - 5);
+
+        for (float _t = 0; _t < 1; _t += (Time.deltaTime / _Rotationfracture))
+        {
+            Object.Instantiate(_bombes, _parent);
+        }
+            
+    }
+
     void Update()
     {
         
-        Quaternion _A = Quaternion.Euler(0, Random.Range(0, 360), 0);
-        Quaternion _B = Quaternion.Euler(0, Random.Range(0, 360), 0);
+        
         for (float _t = 0; _t < 1; _t += (Time.deltaTime/_Rotationfracture)) {
+            Quaternion _A = Quaternion.Euler(0, Random.Range(0, 360), 0);
+            Quaternion _B = Quaternion.Euler(0, Random.Range(0, 360), 0);
             transform.rotation = Quaternion.Slerp(_A,_B, _t);
         }
       
       
     }
     private void FixedUpdate()
-    {
-
-        this.transform.Translate(Vector3. * _speed* Time.deltaTime , Space.World);
+    {    
+        _rgdbdEnnemy2.velocity = new Vector3(_X,0,_Z) * _speed* Time.deltaTime;
     }
 }
--- Unity Cochonation files/Assets/EnemyMovement1.cs
+++ Unity Cochonation files/Assets/EnemyMovement1.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class EnemyMovement1 : MonoBehaviour
 {
-    public Transform _player;
-    public Rigidbody _rigidbodyE;
+    Transform _player;
     public float _speed = 50;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
@@ -22,8 +21,8 @@ public class EnemyMovement1 : MonoBehaviour
 
     private void FixedUpdate()
     {
-        this.transform.LookAt(_player.transform);
-        _rigidbodyE.velocity = transform.forward * _speed;
+       transform.LookAt(_player.position);
+       transform.Translate( -transform.forward * _speed);
     }
     private void OnCollisionEnter(Collision collision)
     {
